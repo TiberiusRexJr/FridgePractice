f709a5848196c0e5f328a53a8eeea6684acf3ac3
IronHead <rouninlopez@gmail.com>
2020-07-07 11:29:50 -0500

    need to bind to models from entires, evenhandlers

---
+++ WPFNavigation_Practice/WPFNavigation_Practice/WPFNavigation_Practice/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WPFNavigation_Practice
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void EntryUserEmail_BindingContextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void  Login(object sender, EventArgs e)
+        {
+            var dash = new NavigationPage(new Dashboard());
+
+            await Navigation.PushAsync(dash);
+        }
+    }
+}
