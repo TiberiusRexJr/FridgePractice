17c091d382805f745b67d3892a7886b25e840114
EVA_1 <rouninlopez@gmail.com>
2020-07-07 18:05:17 -0500

    got bindings down

---
+++ MVVM/MVVM/MVVM/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace MVVM.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainPageViewModel()
+        {
+            var NotesCollection = new ObservableCollection<NoteModel>();
+            
+        }
+    }
+}
---
+++ MVVM/MVVM/MVVM/Views/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MVVM
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            BackgroundColor = Color.PowderBlue;
+            InitializeComponent();
+            BindingContext = new MainPageViewModel();
+
+            var xamagonImage = new Image { Source = "xamagonImage.png" };
+
+            var noteEditor = new Editor
+            {
+                Placeholder = "Enter Note",
+                BackgroundColor = Color.White,
+                Margin = new Thickness(10)
+
+            };
+
+            noteEditor.SetBinding(Editor.TextProperty, "NoteText");
+
+            var saveButton = new Button
+            {
+                Text = "Save",
+                TextColor = Color.White,
+                BackgroundColor = Color.Green,
+                Margin = new Thickness(10)
+            };
+
+            saveButton.SetBinding(Button.CommandProperty, "SaveNoteCommand");
+
+            var deleteButton = new Button
+            {
+                Text = "Delete",
+                TextColor = Color.Red,
+                BackgroundColor = Color.Black,
+                Margin = new Thickness(10)
+
+            };
+            deleteButton.SetBinding(Button.CommandProperty, "DeleteNoteCommand");
+            var collectionView = new CollectionView
+            {
+                ItemTemplate = new NotesTemplate()
+            };
+            collectionView.SetBinding(CollectionView.ItemsSourceProperty, "NotesCollection");
+            var grid = new Grid
+            {
+                Margin=new Thickness(20,40),
+                ColumnDefinitions=
+                { 
+                    new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)},
+                    new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)}
+                },
+                RowDefinitions=
+                { 
+                    new RowDefinition{Height=new GridLength(1.0,GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(2.5,GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(1.0,GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(2.0,GridUnitType.Star)},
+                }
+            };
+            grid.Children.Add(xamagonImage, 0, 0);
+            Grid.SetColumnSpan(xamagonImage, 2);
+            
+            grid.Children.Add(noteEditor, 0, 1);
+            Grid.SetColumnSpan(noteEditor, 2);
+            
+            grid.Children.Add(saveButton, 0, 3);
+            Grid.SetColumnSpan(saveButton, 2);
+
+            grid.Children.Add(deleteButton, 0, 4);
+            Grid.SetColumnSpan(deleteButton, 2);
+
+            
+        }
+    }
+}
